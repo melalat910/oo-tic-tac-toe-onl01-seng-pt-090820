3cafc641b239812253c08ae6a4190780aacc0886
Tirzah P. Howard <tp.howard47@gmail.com>
2020-10-12 05:45:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,32 +2,36 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
+end 
 
-  WIN_COMBINATIONS = [
-  [0,1,2], # top_row
-  [3,4,5], # middle_row
-  [6,7,8], # bottom_row
-  [0,3,6], # left_column
-  [1,4,7], # center_column
-  [2,5,8], # right_column
-  [0,4,8], # left_diagonal
-  [6,4,2] # right_diagonal
+
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
   ]
-
-  def display_board
+  
+    def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-
+  
+  # input_index
+  
   def input_to_index(user_input)
     user_input.to_i - 1
   end
-
+  
   def move(position, char)
-      @board[position] = char
+    @board[position] = char
   end
 
   def position_taken?(index_i)
@@ -37,32 +41,53 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
+  
 
+#turn_count
   def turn_count
-      number_of_turns = 0
-      @board.each do |space|
-      if space == "X" || space == "O"
-          number_of_turns += 1
-      end
+    number_of_turns = 0
+    @board.each do |space|
+    if space == "X" || space == "O"
+        number_of_turns += 1
     end
-    return number_of_turns
-
-  #current_player
-  def current_player
-   turn_count % 2 == 0 ? "X" : "O"
- end
-  #turn
-  def turn
-   puts "Please enter 1-9:"
-   input = gets.strip
-   if valid_move?(input)
-     move(input, current_player)
-   else
-     turn
-   end
-   display_board
-    end
-   end
   end
- end
+  return number_of_turns
+  
+#current_player
+  def current_player
+    if turn_count % 2 == 0
+    "X"
+    else
+    "O"
+  end
+ 
+#turn 
+def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
+end
+
+#won?
+def won?
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
+    end
+      false
+  end
+end
+
+#full?
+def full?
+  @board.all?{|occupied| occupied != " "}
 end
